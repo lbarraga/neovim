ab4269491dd5d53c81fa87f2e09f49d565f994e3
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-06-08 22:32:45 +0200

    add incremental selection

--- config/plugins/lsp-core.nix
+++ config/plugins/lsp-core.nix
@@ -34,5 +34,26 @@
       action = "<cmd>lua vim.diagnostic.open_float()<cr>";
       options.desc = "Line Diagnostics";
     }
+    {
+      mode = "n";
+      key = "<C-space>";
+      action = "van";
+      options.remap = true;
+      options.desc = "Start selection";
+    }
+    {
+      mode = "x";
+      key = "<C-space>";
+      action = "an";
+      options.remap = true;
+      options.desc = "Grow selection";
+    }
+    {
+      mode = "x";
+      key = "<BS>";
+      action = "in";
+      options.remap = true;
+      options.desc = "Shrink selection";
+    }
   ];
 }
