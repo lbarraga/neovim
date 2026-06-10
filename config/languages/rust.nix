f8834a76981f9f27bd2b3c26c0fc6612a7a6ce02
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-06-10 18:01:06 +0200

    add some rust functionality

--- config/languages/rust.nix
+++ config/languages/rust.nix
@@ -1,9 +1,44 @@
 {
-  plugins.lsp.servers.rust_analyzer = {
+  # Rustaceanvim for the language server, type hints, and enabling all cargo features
+  plugins.rustaceanvim = {
     enable = true;
-    installCargo = false;
-    installRustc = false;
-    settings.check.command = "clippy";
+    settings = {
+      server = {
+        default_settings = {
+          "rust-analyzer" = {
+            cargo = {
+              allFeatures = true;
+            };
+            check = {
+              command = "clippy";
+            };
+          };
+        };
+      };
+    };
   };
+
+  # Crates plugin for dependency management
+  plugins.crates = {
+    enable = true;
+  };
+
+  # Add crates to the autocompletion sources
+  plugins.cmp.settings.sources = [
+    {name = "crates";}
+  ];
+
+  # Syntax highlighting
   plugins.treesitter.settings.ensure_installed = ["rust"];
+
+  extraConfigLua = ''
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+          vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+        end
+      end,
+    })
+  '';
 }
