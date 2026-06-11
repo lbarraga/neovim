6b18fa27e0570577cf9327aa65a2133948ca66c7
Lukas Barragan Torres <lukas.barragantorres@ugent.be>
2026-06-11 22:38:05 +0200

    rust improvement

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -1,5 +1,11 @@
-# This file configures the autocomplete popup menu.
 {
+  plugins.lspkind = {
+    enable = true;
+    cmp.enable = true;
+  };
+
+  plugins.luasnip.enable = true;
+
   plugins.cmp = {
     enable = true;
     autoEnableSources = true;
@@ -8,16 +14,23 @@
         completion.border = "rounded";
         documentation.border = "rounded";
       };
+      view = {
+        docs = {
+          auto_open = false;
+        };
+      };
       sources = [
         {name = "nvim_lsp";}
+        {name = "luasnip";}
         {name = "path";}
       ];
       mapping = {
         "<CR>" = "cmp.mapping.confirm({ select = true })";
-        "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-        "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+        "<C-j>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        "<C-k>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
         "<C-Space>" = "cmp.mapping.complete()";
         "<C-e>" = "cmp.mapping.abort()";
+        "<C-h>" = "cmp.mapping(function() local cmp = require('cmp'); if cmp.visible_docs() then cmp.close_docs() else cmp.open_docs() end end)";
       };
     };
   };
